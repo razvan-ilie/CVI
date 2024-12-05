749e3d370abebe079e12c04072143b57454abe7d
Razvan Ilie <razvanil@gmail.com>
2024-12-04 19:40:28 -0500

    updates

---
+++ cvi/option_chain.py
@@ -0,0 +1,26 @@
+import pandera as pa
+from pandera.typing import Series
+
+
+class OptionChain(pa.DataFrameModel):
+    expiry: Series[pa.DateTime] = pa.Field()
+    strike: Series[float] = pa.Field()
+    t_e: Series[float] = pa.Field()
+    c_bid: Series[float] = pa.Field(nullable=True)
+    c_ask: Series[float] = pa.Field(nullable=True)
+    c_mid: Series[float] = pa.Field(nullable=True)
+    p_bid: Series[float] = pa.Field(nullable=True)
+    p_ask: Series[float] = pa.Field(nullable=True)
+    p_mid: Series[float] = pa.Field(nullable=True)
+    fwd_bid: Series[float] = pa.Field(nullable=True)
+    fwd_ask: Series[float] = pa.Field(nullable=True)
+    fwd_mid: Series[float] = pa.Field()
+    disc_bid: Series[float] = pa.Field(nullable=True)
+    disc_ask: Series[float] = pa.Field(nullable=True)
+    disc_mid: Series[float] = pa.Field()
+    iv_c_bid: Series[float] = pa.Field(nullable=True)
+    iv_c_ask: Series[float] = pa.Field(nullable=True)
+    iv_c_mid: Series[float] = pa.Field(nullable=True)
+    iv_p_bid: Series[float] = pa.Field(nullable=True)
+    iv_p_ask: Series[float] = pa.Field(nullable=True)
+    iv_p_mid: Series[float] = pa.Field(nullable=True)
